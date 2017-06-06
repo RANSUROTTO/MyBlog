010bb3e64c7e1dc7a716409ed66b4d91a56d7f68
Joe <578948046@qq.com>
2017-06-06 10:45:03 +0800

    Change settings

--- MyBlog/Blog.Presentation.Web/Global.asax.cs
+++ MyBlog/Blog.Presentation.Web/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -11,6 +12,17 @@ namespace Blog.Presentation.Web
     {
         protected void Application_Start()
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            //禁用 "X-AspNetMvc-Version" 请求头
+            MvcHandler.DisableMvcResponseHeader = true;
+
+            //清除默认视图引擎
+            ViewEngines.Engines.Clear();
+            //仅适用Razor视图引擎
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
