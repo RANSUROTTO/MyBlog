1ea82a8336fa5a335db30396b15eca983eb6bb6d
Joe <578948046@qq.com>
2017-06-07 18:20:44 +0800

    Container Manager

---
+++ MyBlog/Blog.Libraries.Core/Infrastructure/IEngine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Libraries.Core.Infrastructure
+{
+    /// <summary>
+    /// 实现该接口可作为构成提供项目各种服务的引擎
+    /// </summary>
+    public interface IEngine
+    {
+
+
+
+    }
+}
