a47ba94f6cec474302c50bf92b077263b7b65368
Joe <578948046@qq.com>
2017-06-07 18:20:44 +0800

    Container Manager

---
+++ MyBlog/Blog.Libraries.Core/Infrastructure/DependencyManagement/ContainerManager.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using Autofac.Core.Lifetime;
+using Autofac.Integration.Mvc;
+
+namespace Blog.Libraries.Core.Infrastructure.DependencyManagement
+{
+
+    /// <summary>
+    /// 容器管理
+    /// </summary>
+    public class ContainerManager
+    {
+
+        #region Fields
+
+        private readonly IContainer _container;
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="container">容器</param>
+        public ContainerManager(IContainer container)
+        {
+            this._container = container;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// 获得容器
+        /// </summary>
+        public virtual IContainer Container
+        {
+            get
+            {
+                return _container;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        /// <summary>
+        /// 通过指定类型进行解析
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <param name="scope">解析范围; 传递null以自动解析当前作用域</param>
+        /// <returns>解析后的服务</returns>
+        public virtual object Resolve(Type type, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                scope = Scope();
+            }
+            return scope.Resolve(type);
+        }
+
+        /// <summary>
+        /// 通过key值和指定类型进行解析
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="key">键值</param>
+        /// <param name="scope">解析范围; 传递null以自动解析当前作用域</param>
+        /// <returns>解析后的服务</returns>
+        public virtual T Resolve<T>(string key = "", ILifetimeScope scope = null) where T : class
+        {
+            if (scope == null)
+            {
+                scope = Scope();
+            }
+            if (string.IsNullOrEmpty(key))
+            {
+                //不指定键值则获取仅通过指定T类型进行解析
+                return scope.Resolve<T>();
+            }
+            return scope.ResolveKeyed<T>(key);
+        }
+
+
+        /// <summary>
+        /// Resolve all
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="key">key</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved services</returns>
+        public virtual T[] ResolveAll<T>(string key = "", ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            if (string.IsNullOrEmpty(key))
+            {
+                return scope.Resolve<IEnumerable<T>>().ToArray();
+            }
+            return scope.ResolveKeyed<IEnumerable<T>>(key).ToArray();
+        }
+
+        /// <summary>
+        /// Resolve unregistered service
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual T ResolveUnregistered<T>(ILifetimeScope scope = null) where T : class
+        {
+            return ResolveUnregistered(typeof(T), scope) as T;
+        }
+
+        /// <summary>
+        /// Resolve unregistered service
+        /// </summary>
+        /// <param name="type">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual object ResolveUnregistered(Type type, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            var constructors = type.GetConstructors();
+            foreach (var constructor in constructors)
+            {
+                try
+                {
+                    var parameters = constructor.GetParameters();
+                    var parameterInstances = new List<object>();
+                    foreach (var parameter in parameters)
+                    {
+                        var service = Resolve(parameter.ParameterType, scope);
+                        if (service == null) throw new Exception("Unknown dependency");
+                        parameterInstances.Add(service);
+                    }
+                    return Activator.CreateInstance(type, parameterInstances.ToArray());
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+            throw new Exception("No constructor  was found that had all the dependencies satisfied.");
+        }
+
+        /// <summary>
+        /// Try to resolve srevice
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <param name="instance">Resolved service</param>
+        /// <returns>Value indicating whether service has been successfully resolved</returns>
+        public virtual bool TryResolve(Type serviceType, ILifetimeScope scope, out object instance)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            return scope.TryResolve(serviceType, out instance);
+        }
+
+        /// <summary>
+        /// Check whether some service is registered (can be resolved)
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Result</returns>
+        public virtual bool IsRegistered(Type serviceType, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            return scope.IsRegistered(serviceType);
+        }
+
+        /// <summary>
+        /// Resolve optional
+        /// </summary>
+        /// <param name="serviceType">Type</param>
+        /// <param name="scope">Scope; pass null to automatically resolve the current scope</param>
+        /// <returns>Resolved service</returns>
+        public virtual object ResolveOptional(Type serviceType, ILifetimeScope scope = null)
+        {
+            if (scope == null)
+            {
+                //no scope specified
+                scope = Scope();
+            }
+            return scope.ResolveOptional(serviceType);
+        }
+
+        /// <summary>
+        /// Get current scope
+        /// </summary>
+        /// <returns>Scope</returns>
+        public virtual ILifetimeScope Scope()
+        {
+            try
+            {
+                if (HttpContext.Current != null)
+                    return AutofacDependencyResolver.Current.RequestLifetimeScope;
+
+                //when such lifetime scope is returned, you should be sure that it'll be disposed once used (e.g. in schedule tasks)
+                return Container.BeginLifetimeScope(MatchingScopeLifetimeTags.RequestLifetimeScopeTag);
+            }
+            catch (Exception)
+            {
+                //we can get an exception here if RequestLifetimeScope is already disposed
+                //for example, requested in or after "Application_EndRequest" handler
+                //but note that usually it should never happen
+
+                //when such lifetime scope is returned, you should be sure that it'll be disposed once used (e.g. in schedule tasks)
+                return Container.BeginLifetimeScope(MatchingScopeLifetimeTags.RequestLifetimeScopeTag);
+            }
+        }
+    }
+}
---
+++ MyBlog/Blog.Libraries.Core/Infrastructure/EngineContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Libraries.Core.Infrastructure
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EngineContext
+    {
+    }
+}
