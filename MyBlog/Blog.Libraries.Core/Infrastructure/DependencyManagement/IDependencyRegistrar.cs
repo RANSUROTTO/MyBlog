131a7f77ab7946d3f6f241097cccd8dd7541cbb7
Joe <578948046@qq.com>
2017-06-11 17:13:38 +0800

    Engine

---
+++ MyBlog/Blog.Libraries.Core/Infrastructure/DependencyManagement/IDependencyRegistrar.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Blog.Libraries.Core.Configuration;
+using Blog.Libraries.Core.Infrastructure.TypeFinder;
+
+namespace Blog.Libraries.Core.Infrastructure.DependencyManagement
+{
+    /// <summary>
+    /// 依赖注册接口
+    /// </summary>
+    public interface IDependencyRegistrar
+    {
+
+        /// <summary>
+        /// 注册服务的实现
+        /// </summary>
+        /// <param name="builder">容器构建</param>
+        /// <param name="typeFinder">类型查找器</param>
+        /// <param name="config">配置</param>
+        void Register(ContainerBuilder builder, ITypeFinder typeFinder, WebConfig config);
+
+
+        /// <summary>
+        /// 依赖注册顺序
+        /// </summary>
+        int Order { get; }
+
+    }
+}
--- MyBlog/Blog.Libraries.Core/Infrastructure/IEngine.cs
+++ MyBlog/Blog.Libraries.Core/Infrastructure/IEngine.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Blog.Libraries.Core.Configuration;
+using Blog.Libraries.Core.Infrastructure.DependencyManagement;
 
 namespace Blog.Libraries.Core.Infrastructure
 {
@@ -11,8 +9,34 @@ namespace Blog.Libraries.Core.Infrastructure
     /// </summary>
     public interface IEngine
     {
+        /// <summary>
+        /// 容器管理对象
+        /// </summary>
+        ContainerManager ContainerManager { get; }
 
+        /// <summary>
+        /// 初始化运行环境中的组件和插件
+        /// </summary>
+        /// <param name="config">配置</param>
+        void Initialize(WebConfig config);
 
+        /// <summary>
+        /// 解析依赖
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        T Resolve<T>() where T : class;
+
+        /// <summary>
+        ///  解析依赖
+        /// </summary>
+        /// <param name="type">类型</param>
+        object Resolve(Type type);
+
+        /// <summary>
+        /// 解析依赖
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        T[] ResolveAll<T>();
 
     }
 }
