1e69d4c1d3f99a28b16cf9b85935154a72ae38b0
ZhengZicong <578948046@qq.com>
2017-06-12 02:16:44 +0800

    Add a profile class Complete the application service engine Add log.txt

---
+++ MyBlog/Blog.Libraries.Core/Configuration/BaseConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Xml;
+
+namespace Blog.Libraries.Core.Configuration
+{
+    public abstract class BaseConfig<T> : IConfigurationSectionHandler
+    {
+        protected T Config;
+
+        public abstract T Create(XmlNode section);
+
+        public object Create(object parent, object configContext, XmlNode section)
+        {
+            return Create(section);
+        }
+
+    }
+}
