7622c061745aa84d2f81d306f4db21ecd324ff5b
ZhengZicong <578948046@qq.com>
2017-07-19 00:55:55 +0800

    add unit test

---
+++ MyBlog/Blog.Tests/TestsBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Principal;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Blog.Tests
+{
+    public abstract class TestsBase
+    {
+
+        protected MockRepository mocks;
+
+        /// <summary>
+        /// 代表每个测试方法前先调用该方法实例化RhinoMocks对象
+        /// </summary>
+        [SetUp]
+        public virtual void SetUp()
+        {
+            mocks = new MockRepository();
+        }
+
+        /// <summary>
+        /// 代表每个测试方法结束后调用该拆卸方法
+        /// </summary>
+        [TearDown]
+        public virtual void TearDown()
+        {
+            if (mocks != null)
+            {
+                mocks.ReplayAll();
+                mocks.VerifyAll();
+            }
+        }
+
+        protected static IPrincipal CreatePrincipal(string name, params string[] roles)
+        {
+            return new GenericPrincipal(new GenericIdentity(name, "TestIdentity"), roles);
+        }
+
+    }
+}
---
+++ MyBlog/Blog.Tests/TypeAssert.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Blog.Tests
+{
+    /// <summary>
+    /// 包含用于单元测试的类型断言
+    /// </summary>
+    public static class TypeAssert
+    {
+
+        /// <summary>
+        /// 检查两个对象类型是否相等
+        /// </summary>
+        /// <param name="expected">预期类型对象</param>
+        /// <param name="instance">实例类型对象</param>
+        public static void AreEqual(object expected, object instance)
+        {
+            if (expected == null)
+                Assert.IsNull(instance);
+            else
+                Assert.IsNotNull(instance, "Instance was null");
+            Assert.AreEqual(expected.GetType(), instance.GetType(), "Expected: " + expected.GetType() + ", was: " + instance.GetType() + " was not of type " + instance.GetType());
+        }
+
+        /// <summary>
+        /// 检查对象是否匹配预期类型
+        /// </summary>
+        /// <param name="expected">预期类型</param>
+        /// <param name="instance">实例类型对象</param>
+        public static void AreEqual(Type expected, object instance)
+        {
+            if (expected == null)
+                Assert.IsNull(instance);
+            else
+                Assert.IsNotNull(instance, "Instance was null");
+            Assert.AreEqual(expected, instance.GetType(), "Expected: " + expected + ", was: " + instance.GetType() + " was not of type " + instance.GetType());
+        }
+
+        /// <summary>
+        /// 检查对象是否匹配泛型预期类型T
+        /// </summary>
+        /// <typeparam name="T">预期类型</typeparam>
+        /// <param name="instance">实例类型对象</param>
+        public static void Equals<T>(object instance)
+        {
+            AreEqual(typeof(T), instance);
+        }
+
+        /// <summary>
+        /// 检查实例类型是否派生于泛型预期类型T
+        /// </summary>
+        /// <typeparam name="T">预期类型</typeparam>
+        /// <param name="instance">实例类型对象</param>
+        public static void Is<T>(object instance)
+        {
+            Assert.IsTrue(instance is T, "Instance " + instance + " was not of type " + typeof(T));
+        }
+
+    }
+}
