17161a5edb1f53aee6060db3cf9396081be4f594
PROGRAMME <PROGRAMME@PC-ZICONG>
2017-07-21 01:39:01 +0800

    add unit test

--- MyBlog/Blog.Tests/AttributeExtensions.cs
+++ MyBlog/Blog.Tests/AttributeExtensions.cs
@@ -1,39 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Blog.Tests
 {
 
+    /// <summary>
+    /// 自定义特性的扩展类
+    /// </summary>
     public static class AttributeExtensions
     {
 
         /// <summary>
-        /// 如果属性attributeTarget使用了TAttribute类进行修饰,则返回true
+        /// 如果attributeTarget对象使用了TAttribute类进行修饰,则返回true
         /// 没有则返回false
         /// </summary>
         /// <typeparam name="TAttribute">预期修饰的Attribute类</typeparam>
-        /// <param name="attributeTarget">属性</param>
-        /// <returns></returns>
+        /// <param name="attributeTarget">对象</param>
         public static bool IsDecoratedWith<TAttribute>(this ICustomAttributeProvider attributeTarget) where TAttribute : Attribute
         {
             return attributeTarget.GetCustomAttributes(typeof(TAttribute), false).Length > 0;
         }
 
         /// <summary>
-        /// Will return true the first attribute of type TAttribute on the attributeTarget.
+        /// 返回修饰attributeTarget对象所用的TAttribute类对象
         /// </summary>
-        /// <typeparam name="TAttribute"></typeparam>
-        /// <param name="attributeTarget"></param>
-        /// <returns></returns>
+        /// <typeparam name="TAttribute">预期修饰的Attribute类</typeparam>
+        /// <param name="attributeTarget">对象</param>
         public static TAttribute GetAttribute<TAttribute>(this ICustomAttributeProvider attributeTarget) where TAttribute : Attribute
         {
             return (TAttribute)attributeTarget.GetCustomAttributes(typeof(TAttribute), false)[0];
         }
+
+    }
+
+    /// <summary>
+    /// 针对自定义特性的扩展类的单元测试
+    /// </summary>
+    [TestFixture]
+    public class AttributeExtensionsTests
+    {
+
+        #region Attribute and Model
+
+        public class AttrTestAttribute : Attribute { }
+        public class FailAttrTestAttribute : Attribute { }
+
+        public class AttrTestModel
+        {
+            [AttrTest]
+            public string Name { get; set; }
+        }
+
+        #endregion
+
+        #region UnitTest AttributeExtensions.IsDecoratedWith_Generice
+
+        [Test]
+        public void PassesOnAttributeAny_generice()
+        {
+            Type classType = typeof(AttrTestModel);
+            PropertyInfo property = classType.GetProperty("Name");
+            bool flag = property.IsDecoratedWith<AttrTestAttribute>();
+            Assert.AreEqual(true, flag);
+        }
+
+        [Test]
+        [Ignore("这是一个单元测试结果为失败的方法")]
+        public void ReturnTheAttributeEquals_generice_fail()
+        {
+            Type classType = typeof(AttrTestModel);
+            PropertyInfo property = classType.GetProperty("Name");
+            bool flag = property.IsDecoratedWith<FailAttrTestAttribute>();
+            Assert.AreEqual(true, flag);
+        }
+
+        #endregion
+
+        #region UnitTest AttributeExtensions.GetAttribute_Generice
+
+        [Test]
+        public void PassesOnAttributeGet_generice()
+        {
+            Type classType = typeof(AttrTestModel);
+            PropertyInfo property = classType.GetProperty("Name");
+            var result = property.GetAttribute<AttrTestAttribute>();
+
+            TypeAssert.AreEqual(typeof(AttrTestAttribute), result);
+        }
+
+        #endregion
 
     }
 
