38d5f22bd1373b1b6794e61cf84407328cd17777
ZhengZicong <578948046@qq.com>
2017-07-29 23:21:24 +0800

    add plugins engine

---
+++ MyBlog/Blog.Libraries.Core/Plugins/IPlugin.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Blog.Libraries.Core.Plugins
+{
+
+    /// <summary>
+    /// 代表整个插件的属性
+    /// </summary>
+    public interface IPlugin
+    {
+
+        /// <summary>
+        /// 插件模型
+        /// </summary>
+        PluginDescriptor PluginDescriptor { get; set; }
+
+        /// <summary>
+        /// 安装插件
+        /// </summary>
+        void Install();
+
+        /// <summary>
+        /// 卸载插件
+        /// </summary>
+        void Uninstall();
+
+    }
+
+}
---
+++ MyBlog/Blog.Libraries.Core/Plugins/PluginDescriptor.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Libraries.Core.Plugins
+{
+
+    /// <summary>
+    /// 插件模型
+    /// </summary>
+    public class PluginDescriptor : IComparable<PluginDescriptor>
+    {
+
+        #region Ctor
+
+        public PluginDescriptor()
+        {
+            this.SupportedVersions = new List<string>();
+            this.LimitedToStores = new List<int>();
+            this.LimitedToCustomerRoles = new List<int>();
+        }
+
+        public PluginDescriptor(Assembly referencedAssembly, FileInfo originalAssemblyFile,
+            Type pluginType)
+            : this()
+        {
+            this.ReferencedAssembly = referencedAssembly;
+            this.OriginalAssemblyFile = originalAssemblyFile;
+            this.PluginType = pluginType;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Plugin type
+        /// </summary>
+        public virtual string PluginFileName { get; set; }
+
+        /// <summary>
+        /// 插件类型
+        /// </summary>
+        public virtual Type PluginType { get; set; }
+
+        /// <summary>
+        /// The assembly that has been shadow copied that is active in the application
+        /// </summary>
+        public virtual Assembly ReferencedAssembly { get; internal set; }
+
+        /// <summary>
+        /// The original assembly file that a shadow copy was made from it
+        /// </summary>
+        public virtual FileInfo OriginalAssemblyFile { get; internal set; }
+
+        /// <summary>
+        /// 插件组
+        /// </summary>
+        public virtual string Group { get; set; }
+
+        /// <summary>
+        /// 友好名称
+        /// </summary>
+        public virtual string FriendlyName { get; set; }
+
+        /// <summary>
+        /// 系统名称
+        /// </summary>
+        public virtual string SystemName { get; set; }
+
+        /// <summary>
+        /// 版本
+        /// </summary>
+        public virtual string Version { get; set; }
+
+        /// <summary>
+        /// 支持的版本
+        /// </summary>
+        public virtual IList<string> SupportedVersions { get; set; }
+
+        /// <summary>
+        /// 作者
+        /// </summary>
+        public virtual string Author { get; set; }
+
+        /// <summary>
+        /// 描述信息
+        /// </summary>
+        public virtual string Description { get; set; }
+
+        /// <summary>
+        /// 显示顺序
+        /// </summary>
+        public virtual int DisplayOrder { get; set; }
+
+        /// <summary>
+        /// 是否已安装
+        /// </summary>
+        public virtual bool Installed { get; set; }
+
+
+
+        /// <summary>
+        /// Gets or sets the list of store identifiers in which this plugin is available. If empty, then this plugin is available in all stores
+        /// </summary>
+        public virtual IList<int> LimitedToStores { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of customer role identifiers for which this plugin is available. If empty, then this plugin is available for all ones.
+        /// </summary>
+        public virtual IList<int> LimitedToCustomerRoles { get; set; }
+
+
+        #endregion
+
+
+
+        #region Utilities
+
+        /// <summary>
+        /// 实现该方法可以帮助PluginDescriptor集合进行排序
+        /// </summary>
+        public int CompareTo(PluginDescriptor other)
+        {
+            if (DisplayOrder != other.DisplayOrder)
+                return DisplayOrder.CompareTo(other.DisplayOrder);
+
+            return string.Compare(FriendlyName, other.FriendlyName, StringComparison.Ordinal);
+        }
+
+        public override string ToString()
+        {
+            return FriendlyName;
+        }
+
+        public override int GetHashCode()
+        {
+            return SystemName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as PluginDescriptor;
+            return other != null && SystemName != null && SystemName.Equals(other.SystemName);
+        }
+
+        #endregion
+
+
+    }
+}
