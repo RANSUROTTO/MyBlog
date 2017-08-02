a9d81ae183ec2b3645bc3a152f5faac89944e132
ZhengZicong <578948046@qq.com>
2017-08-03 07:30:37 +0800

    add datasetting.cs

---
+++ MyBlog/Blog.Libraries.Core/Data/DataSettings.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Blog.Libraries.Core.Data
+{
+
+    /// <summary>
+    /// 数据源设置 (连接字符串信息)
+    /// </summary>
+    public class DataSettings
+    {
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public DataSettings()
+        {
+            RawDataSettings = new Dictionary<string, string>();
+        }
+
+        /// <summary>
+        /// 数据源提供商  ../mysql,sqlserver,oracle
+        /// </summary>
+        public string DataProvider { get; set; }
+
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        public string DataConnectionString { get; set; }
+
+        /// <summary>
+        /// 格式化数据源信息
+        /// </summary>
+        public IDictionary<string, string> RawDataSettings { get; private set; }
+
+        /// <summary>
+        /// 获取一个值,指示数据源连接信息是否有效
+        /// </summary>
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(this.DataProvider) && !string.IsNullOrEmpty(this.DataConnectionString);
+        }
+
+    }
+
+}
