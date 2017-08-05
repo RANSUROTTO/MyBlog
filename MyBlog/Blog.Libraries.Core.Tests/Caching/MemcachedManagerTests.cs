b46f13b041396dd22c61144a72dc5ce1d54fea8a
ZhengZicong <578948046@qq.com>
2017-08-06 00:47:04 +0800

    complete memcached manager unit tests

--- MyBlog/Blog.Libraries.Core.Tests/Caching/MemcachedManagerTests.cs
+++ MyBlog/Blog.Libraries.Core.Tests/Caching/MemcachedManagerTests.cs
@@ -1,19 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using Blog.Libraries.Core.Caching.MemCaching;
 using Blog.Tests;
 
 namespace Blog.Libraries.Core.Tests.Caching
 {
 
+    [Ignore("Please configure enyim.memcached in app.config")]
     [TestFixture]
     public class MemcachedManagerTests
     {
 
+        #region Field
 
+        private static MemcachedManager _client;
+
+        #endregion
+
+        #region Ctor
+
+        static MemcachedManagerTests()
+        {
+            _client = new MemcachedManager("enyim.com/memcached");
+        }
+
+        #endregion
+
+        [Test]
+        public void PassesMamcachedManager_Set_Success()
+        {
+            _client.Set("name", "joe", 1000);
+            _client.HasKey("name").TestBeTrue();
+            _client.Get<string>("name").TestEqual("joe");
+            _client.Remove("name");
+            _client.HasKey("name").TestBeFalse();
+        }
+
+        [Test]
+        public void PassesMemcachedManager_Set_Success()
+        {
+            _client.Set("key1", "value1", 1000);
+            _client.Set("key2", "value2", 1000);
+            _client.HasKey("key1").TestBeTrue();
+            _client.HasKey("key2").TestBeTrue();
+            _client.Clear();
+            _client.HasKey("key1").TestBeFalse();
+            _client.HasKey("key2").TestBeFalse();
+
+        }
 
     }
 
