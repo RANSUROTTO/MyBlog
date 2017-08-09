b507fcf9e07e54ba6df9f6bb09d020be83187c14
ZhengZicong <578948046@qq.com>
2017-08-10 00:57:45 +0800

    add GenericListTypeConverter

---
+++ MyBlog/Blog.Libraries.Core/ComponentModel/GenericListTypeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Libraries.Core.ComponentModel
+{
+
+    /// <summary>
+    /// 泛型集合类型转换器
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenericListTypeConverter<T> : TypeConverter
+    {
+
+        protected readonly TypeConverter typeConverter;
+
+        public GenericListTypeConverter()
+        {
+            typeConverter = TypeDescriptor.GetConverter(typeof(T));
+            if (typeConverter == null)
+                throw new InvalidOperationException("No type converter exists for type " + typeof(T).FullName);
+        }
+
+
+
+
+    }
+
+}
