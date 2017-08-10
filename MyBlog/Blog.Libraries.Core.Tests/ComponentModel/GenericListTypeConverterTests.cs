16cc4f5de4f2f2e37f09eb05645163638506ba53
ZhengZicong <578948046@qq.com>
2017-08-11 01:19:18 +0800

    complete GenericListTypeConverter Add GenericListTypeConverter unite test

---
+++ MyBlog/Blog.Libraries.Core.Tests/ComponentModel/GenericListTypeConverterTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Blog.Tests;
+
+namespace Blog.Libraries.Core.Tests.ComponentModel
+{
+
+    [TestFixture]
+    public class GenericListTypeConverterTests
+    {
+
+    }
+
+}
--- MyBlog/Blog.Libraries.Core/ComponentModel/GenericListTypeConverter.cs
+++ MyBlog/Blog.Libraries.Core/ComponentModel/GenericListTypeConverter.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blog.Libraries.Core.ComponentModel
 {
@@ -24,8 +23,72 @@ namespace Blog.Libraries.Core.ComponentModel
                 throw new InvalidOperationException("No type converter exists for type " + typeof(T).FullName);
         }
 
+        /// <summary>
+        /// 从逗号分割的字符串获取字符串数组
+        /// </summary>
+        /// <param name="input">字符串</param>
+        /// <returns>分割后的字符串数组</returns>
+        protected virtual string[] GetStringByArray(string input)
+        {
+            return string.IsNullOrEmpty(input) ? new string[0] : input.Split(',').Select(p => p.Trim()).ToArray();
+        }
 
+        /// <summary>
+        /// 获取一个值，表示该转换器是否可以
+        /// 将给定源类型中的对象转换为转换器的本机类型
+        /// 使用上下文
+        /// </summary>
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                string[] items = GetStringByArray(sourceType.ToString());
+                return items.Any();
+            }
+            return base.CanConvertFrom(context, sourceType);
+        }
 
+        /// <summary>
+        /// 将给定对象转换为转换器的本机类型
+        /// </summary>
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            if (value is string)
+            {
+                var items = GetStringByArray((string)value);
+                var result = new List<T>();
+                Array.ForEach(items, p =>
+                {
+                    object item = typeConverter.ConvertFromInvariantString(p);
+                    if (item != null) result.Add((T)item);
+                });
+                return result;
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        /// <summary>
+        /// 使用指定的上下文和参数将给定值对象转换为指定的目标类型
+        /// </summary>
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType == typeof(string))
+            {
+                string result = string.Empty;
+                if (value != null)
+                {
+                    for (int i = 0; i < ((IList<T>)value).Count; i++)
+                    {
+                        var str = Convert.ToString(((IList<T>)value)[i], CultureInfo.InvariantCulture);
+                        result += str;
+                        if (i != ((IList<T>)value).Count - 1)
+                            result += ",";
+                    }
+                }
+                return result;
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
+        }
 
     }
 
