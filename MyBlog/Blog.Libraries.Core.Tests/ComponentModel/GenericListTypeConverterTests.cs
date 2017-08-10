3164cad7bb21c865325bd95fdc1f97714bfe3d1b
ZhengZicong <578948046@qq.com>
2017-08-11 02:04:40 +0800

    change genericeListTypeConvert unit test

--- MyBlog/Blog.Libraries.Core.Tests/ComponentModel/GenericListTypeConverterTests.cs
+++ MyBlog/Blog.Libraries.Core.Tests/ComponentModel/GenericListTypeConverterTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.ComponentModel;
+using Blog.Libraries.Core.ComponentModel;
 using Blog.Tests;
 
 namespace Blog.Libraries.Core.Tests.ComponentModel
@@ -12,6 +14,20 @@ namespace Blog.Libraries.Core.Tests.ComponentModel
     [TestFixture]
     public class GenericListTypeConverterTests
     {
+
+        [SetUp]
+        public void SetUp()
+        {
+            TypeDescriptor.AddAttributes(typeof(List<int>),
+                new TypeConverterAttribute(typeof(GenericListTypeConverter<int>)));
+
+            TypeDescriptor.AddAttributes(typeof(List<string>),
+                new TypeConverterAttribute(typeof(GenericListTypeConverter<string>)));
+        }
+
+
+
+
 
     }
 
