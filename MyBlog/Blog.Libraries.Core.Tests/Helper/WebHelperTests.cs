a3d9b6bff3f85f9eb13cfaba6cc283ba5b2b528b
ZhengZicong <578948046@qq.com>
2017-08-16 01:22:47 +0800

    change webhelper unit test

--- MyBlog/Blog.Libraries.Core.Tests/Helper/WebHelperTests.cs
+++ MyBlog/Blog.Libraries.Core.Tests/Helper/WebHelperTests.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Blog.Libraries.Core.Helper;
 using System.Web;
+using Blog.Libraries.Core.Fakes;
 using NUnit.Framework;
+using Blog.Tests;
 
 namespace Blog.Libraries.Core.Tests.Helper
 {
@@ -17,7 +20,19 @@ namespace Blog.Libraries.Core.Tests.Helper
         private HttpContextBase _httpContext;
         private IWebHelper _webHelper;
 
-        //public void Passes
+        [Test]
+        public void Passes_ServerVariables_Get_Success()
+        {
+            var serverVariables = new NameValueCollection();
+            serverVariables.Add("key1", "value1");
+            serverVariables.Add("key2", "value2");
+            _httpContext = new FakeHttpContext("~/", "Get", null, null, null, null, null, serverVariables);
+            _webHelper = new WebHelper(_httpContext);
+            //Tests
+            _webHelper.ServerVariables("key1").TestEqual("value1");
+            _webHelper.ServerVariables("key2").TestEqual("value2");
+            _webHelper.ServerVariables("key3").TestEqual(string.Empty);
+        }
 
 
 
