47a23142179c6a151879367e1b60df917d3bfae9
ZhengZicong <578948046@qq.com>
2017-08-22 23:56:26 +0800

    add genericeDictionaryTypeConverter unit tests

---
+++ MyBlog/Blog.Libraries.Core.Tests/ComponentModel/GenericeDictionaryTypeConverterTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Libraries.Core.ComponentModel;
+using Blog.Tests;
+
+namespace Blog.Libraries.Core.Tests.ComponentModel
+{
+
+    public class GenericeDictionaryTypeConverterTests
+    {
+
+        public void SetUp()
+        {
+            TypeDescriptor.AddAttributes(typeof(Dictionary<string, string>)
+                , new TypeConverterAttribute(typeof(GenericeDictionaryTypeConverter<string, string>)));
+            TypeDescriptor.AddAttributes(typeof(Dictionary<int, string>)
+                , new TypeConverterAttribute(typeof(GenericeDictionaryTypeConverter<int, string>)));
+        }
+
+        public void Passes_DictionaryConverter_Equal_GenericeDictionaryTypeConverterStringKeyStringValue()
+        {
+            var converter = TypeDescriptor.GetConverter(typeof(Dictionary<string, string>));
+            converter.GetType().TestEqual(typeof(GenericeDictionaryTypeConverter<string, string>));
+        }
+
+
+    }
+
+}
