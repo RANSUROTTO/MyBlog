1b044fbfdd3c9a28017867e3edd9c3becb33c5f2
ZhengZicong <578948046@qq.com>
2017-08-23 23:24:36 +0800

    change members folder

---
+++ MyBlog/Blog.Libraries.Core/Domain/Members/Admin.cs
@@ -0,0 +1,11 @@
+﻿using Blog.Libraries.Core.Data;
+
+namespace Blog.Libraries.Core.Domain.Members
+{
+
+    public class Admin : BaseEntity
+    {
+
+    }
+
+}
---
+++ MyBlog/Blog.Libraries.Core/Domain/Members/Guest.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Libraries.Core.Data;
+
+namespace Blog.Libraries.Core.Domain.Members
+{
+
+    /// <summary>
+    /// 代表一个游客
+    /// </summary>
+    public class Guest : BaseEntity
+    {
+
+    }
+
+}
