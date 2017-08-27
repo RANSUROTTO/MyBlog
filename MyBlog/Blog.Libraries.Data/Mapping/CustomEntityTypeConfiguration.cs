dc6e331b7c6ed8a611e1f2fdaf104d709d1d72f4
ZhengZicong <578948046@qq.com>
2017-08-27 23:03:16 +0800

    Add LogMap.cs

---
+++ MyBlog/Blog.Libraries.Data/Mapping/CustomEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Blog.Libraries.Core.Data;
+
+namespace Blog.Libraries.Data.Mapping
+{
+
+    public abstract class CustomEntityTypeConfiguration<T> : EntityTypeConfiguration<T> where T : BaseEntity
+    {
+
+        protected CustomEntityTypeConfiguration()
+        {
+            this.Initialize();
+        }
+
+        /// <summary>
+        /// 可以重写这一个方法
+        /// 实现通过构造函数执行该自定义初始化代码
+        /// </summary>
+        protected virtual void Initialize()
+        { }
+
+    }
+
+}
---
+++ MyBlog/Blog.Libraries.Data/Mapping/Loggin/LogMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Libraries.Data.Domain.Logging;
+
+namespace Blog.Libraries.Data.Mapping.Loggin
+{
+
+    public class LogMap : CustomEntityTypeConfiguration<Log>
+    {
+
+        public LogMap()
+        {
+            this.ToTable("Log");
+            this.HasKey(p => p.Id);
+            this.Property(p => p.ShortMessage).IsRequired();
+            this.Property(p => p.IpAddress).HasMaxLength(200);
+            
+
+        }
+
+    }
+
+}
