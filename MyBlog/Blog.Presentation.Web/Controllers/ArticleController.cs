0a2883baabf16266efb0625f0e35351c0800b52b
ZhengZicong <578948046@qq.com>
2017-09-10 22:16:03 +0800

    add editor.d

---
+++ MyBlog/Blog.Presentation.Web/Controllers/ArticleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Presentation.Web.Controllers
+{
+    public class ArticleController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
