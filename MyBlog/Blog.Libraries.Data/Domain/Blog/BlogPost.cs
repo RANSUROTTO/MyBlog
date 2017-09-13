56b568cba04ca76c053217300f83e1bd99abed29
ZhengZicong <578948046@qq.com>
2017-09-13 23:51:31 +0800

    alter blogpost

--- MyBlog/Blog.Libraries.Data/Domain/Blog/BlogPost.cs
+++ MyBlog/Blog.Libraries.Data/Domain/Blog/BlogPost.cs
@@ -5,20 +5,36 @@ namespace Blog.Libraries.Data.Domain.Blog
 {
 
     /// <summary>
-    /// 代表一个文章
+    /// 代表一个博文
     /// </summary>
     public class BlogPost : BaseEntity
     {
 
         /// <summary>
-        /// 获取或设置文章标题
+        /// 获取或设置博文标题
         /// </summary>
         public string Title { get; set; }
 
         /// <summary>
-        /// 获取或设置文章内容
+        /// 获取或设置博文内容
         /// </summary>
-        public string Content { get; set; }
+        public string Body { get; set; }
+
+        /// <summary>
+        /// 获取或设置博文的概述
+        /// 如果不为空,展示时则不显示博文内容,而是显示概述
+        /// </summary>
+        public string BodyOverview { get; set; }
+
+        /// <summary>
+        /// 获取或设置一个值,指示是否允许评论
+        /// </summary>
+        public bool AllowComments { get; set; }
+
+
+
+
+
 
         /// <summary>
         /// 获取或设置文章作者
---
+++ MyBlog/Blog.Libraries.Data/Settings/BlogSettings.cs
@@ -0,0 +1,29 @@
+﻿using Blog.Libraries.Core.Configuration;
+
+namespace Blog.Libraries.Data.Settings
+{
+
+    /// <summary>
+    /// 系统对博客的设置
+    /// </summary>
+    public class BlogSettings : ISettings
+    {
+
+        /// <summary>
+        /// 获取或设置一个值,指示游客是否可以进行评论
+        /// </summary>
+        public bool AllowGuestToLeaveComments { get; set; }
+
+        /// <summary>
+        /// 获取或设置一个值,指示博文可有的标签数
+        /// </summary>
+        public int NumberOfTag { get; set; }
+
+        /// <summary>
+        /// 获取或设置一个值,指示是否显示博客的RSS链接
+        /// </summary>
+        public bool ShowHeaderRssUrl { get; set; }
+
+    }
+
+}
