44650a7c4fa947f13cf33d7b926f43ae7b923bcc
Joe <578948046@qq.com>
2017-10-19 09:33:44 +0800

    add settings entity

---
+++ MyBlog/Blog.Libraries.Data/Domain/Configuration/Setting.cs
@@ -0,0 +1,37 @@
+﻿using Blog.Libraries.Core.Data;
+
+namespace Blog.Libraries.Data.Domain.Configuration
+{
+
+    public class Setting : BaseEntity
+    {
+
+        public Setting()
+        {
+        }
+
+        public Setting(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        /// <summary>
+        /// 获取或设置名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 获取或设置值
+        /// </summary>
+        public string Value { get; set; }
+
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+    }
+
+}
