7045f44ff5ef12964b102a6c2c13760565c8deab
ZhengZicong <578948046@qq.com>
2017-11-08 00:34:06 +0800

    update

---
+++ MyBlog/Blog.Presentation.Web/Applications/Blog.Presentation.Admin/Controllers/RegionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Presentation.Admin.Controllers
+{
+    public class RegionController : Controller
+    {
+        // GET: Region
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
