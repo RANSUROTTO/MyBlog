0da8ade54807d3b1ce9ead4b9cc67a1041aa6977
ZhengZicong <578948046@qq.com>
2017-11-08 22:15:41 +0800

    update

---
+++ MyBlog/Blog.Presentation.Framework/Routes/RoutePublisher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Routing;
+using Blog.Libraries.Core.Infrastructure.TypeFinder;
+
+namespace Blog.Presentation.Framework.Routes
+{
+
+    /// <summary>
+    /// 路由发布器
+    /// </summary>
+    public class RoutePublisher
+    {
+
+
+    }
+
+}
