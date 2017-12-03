06b8d70d16a82cbf3fcacf54ab4c7ffefb52cdca
ZhengZicong <578948046@qq.com>
2017-12-03 21:04:44 +0800

    update

---
+++ MyBlog/Blog.Presentation.Framework/Attributes/ControllerDescription.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Blog.Presentation.Framework.Attributes
+{
+
+    /// <summary>
+    /// 代表一个控制器的描述信息特性
+    /// </summary>
+    public class ControllerDescription : Attribute
+    {
+        public ControllerDescription()
+        {
+        }
+    }
+
+}
