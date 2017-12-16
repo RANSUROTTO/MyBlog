75985d22012d72f4c4081b479934f59a39255455
Joe <578948046@qq.com>
2017-12-16 11:49:01 +0800

    update

--- MyBlog/Blog.Presentation.Framework/Services/Controller/AdminControllerService.cs
+++ MyBlog/Blog.Presentation.Framework/Services/Controller/AdminControllerService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Blog.Libraries.Core.Infrastructure.TypeFinder;
 using Blog.Presentation.Framework.Attributes;
@@ -16,7 +17,7 @@ namespace Blog.Presentation.Framework.Services.Controller
 
         private readonly ITypeFinder _typeFinder;
         private readonly IRoleService _roleService;
-        
+
 
         #endregion
 
@@ -46,9 +47,10 @@ namespace Blog.Presentation.Framework.Services.Controller
                 if (descriptionAttribute == null)
                     continue;
 
+                var currentMenus = adminMenus;
                 foreach (var description in descriptionAttribute.Descriptions)
                 {
-
+                    currentMenus = IterationToMenu(currentMenus, description);
                 }
 
             }
@@ -59,7 +61,22 @@ namespace Blog.Presentation.Framework.Services.Controller
 
         #region Utilities
 
+        private List<AdminMenu> IterationToMenu(List<AdminMenu> menuList, ControllerDescriptionAttribute.ControllerDescription description)
+        {
+            if (menuList.Any(p => p.Name == description.Name))
+                return menuList.First(p => p.Name == description.Name).Children.ToList();
 
+            var adminMenum = new AdminMenu
+            {
+                Name = description.Name,
+                I18N = description.I18N,
+                Icon = description.Icon,
+                Order = description.Order,
+                Children = new List<AdminMenu>()
+            };
+            menuList.Add(adminMenum);
+            return adminMenum.Children.ToList();
+        }
 
         #endregion
 
