ba53e08dfd88c7c32a9ae049b22855367df4914a
ZhengZicong <578948046@qq.com>
2017-12-17 14:11:29 +0800

    update

---
+++ MyBlog/Blog.Presentation.Web/Applications/Blog.Presentation.Admin/Controllers/BlogPostController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Blog.Presentation.Framework.Attributes;
+
+namespace Blog.Presentation.Admin.Controllers
+{
+
+    [ControllerDescription("", "")]
+    public class BlogPostController : Controller
+    {
+
+
+    }
+
+}
