f03e61ef6d2f4fccbc1745471d58214d84f60bd8
ZhengZicong <578948046@qq.com>
2017-12-27 16:59:40 +0800

    update

--- MyBlog/Blog.Presentation.Web/Applications/Blog.Presentation.Admin/Controllers/BlogPostController.cs
+++ MyBlog/Blog.Presentation.Web/Applications/Blog.Presentation.Admin/Controllers/BlogPostController.cs
@@ -6,14 +6,26 @@ using Blog.Presentation.Framework.Controllers;
 namespace Blog.Presentation.Admin.Controllers
 {
 
-    [ControllerDescription("[Name=test,Icon=fa fa-awit,Order=0,I18n=true]", "[Name=abc,Icon=fa fa-awit,Order=0,I18n=true]")]
-    public class BlogPostController : AdminController 
+    [ControllerDescription(
+        "[Name=博文管理,Icon=fa fa-awit,Order=0,I18n=true]"
+        , "[Name=文章管理,Icon=fa fa-awit,Order=0,I18n=true]"
+        )]
+    public class BlogPostController : AdminController
     {
 
         #region Constructor
 
         public BlogPostController(IWorkContext workContext) : base(workContext)
         {
+        }
+
+        #endregion
+
+        #region Methods
+
+        public ActionResult Index()
+        {
+            return View();
         }
 
         #endregion
