438be66895ee6213897d9ac4c73544798bb89c29
ZhengZicong <578948046@qq.com>
2018-01-23 09:25:53 +0800

    update

--- MyBlog/Blog.Libraries.Services/Service.cs
+++ MyBlog/Blog.Libraries.Services/Service.cs
@@ -3,5 +3,8 @@
 
 
 
+Name: Language, ModelType: Blog.Libraries.Core.Domain.Localization.Language, ModuleName: Localization, UseModule: True
+Name: LocaleStringResource, ModelType: Blog.Libraries.Core.Domain.Localization.LocaleStringResource, ModuleName: Localization, UseModule: True
+
 E:\Code Repository\MyBlog\MyBlog\  
 E:\Code Repository\MyBlog\MyBlog\Blog.Libraries.Services\  
